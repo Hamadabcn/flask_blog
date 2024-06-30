81e838aa68fcb7a932415e82bde46256f1694cac
Hamada <131229925+Hamadabcn@users.noreply.github.com>
2024-06-30 17:56:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from auth import bp as authbp
 
 app = Flask(__name__)
 
-app.config.from_mapping(SECRET_KEY='a;lskmnbghjljhgf;lkvm;zxlkjsdf890u')
+app.config.from_mapping(SECRET_KEY='Your Secret Key')
 
 app.register_blueprint(blogbp)
 app.register_blueprint(authbp)
